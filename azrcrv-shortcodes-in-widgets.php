e6cc8ec74df2fd55fec1b317f6ef6826b22d3b13
azurecurve <github@azurecurve.co.uk>
2022-04-03 09:44:34 +0100

    * Update readme files.  * Update language template.  * Fix bug with azurecurve menu.

--- azrcrv-shortcodes-in-widgets.php
+++ azrcrv-shortcodes-in-widgets.php
@@ -3,7 +3,7 @@
  * ------------------------------------------------------------------------------
  * Plugin Name: Shortcodes in Widgets
  * Description: Allows shortcodes to be used in widgets
- * Version: 1.2.2
+ * Version: 1.2.3
  * Author: azurecurve
  * Author URI: https://development.azurecurve.co.uk/classicpress-plugins/
  * Plugin URI: https://development.azurecurve.co.uk/classicpress-plugins/shortcodes-in-widgets/
